6a47269d24e9cef370972fc2c3342d60cbd210bd
akdogan <c.a.akdogan@googlemail.com>
2020-10-28 14:04:15 +0100

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,44 @@
+import kotlin.random.Random
+val range = 1..50
+const val maxTries = 5
+
+fun main(args: Array<String>) {
+    evaluation(playGuessingGame())
+}
+
+fun playGuessingGame(): Boolean{
+    val numberToGuess = Random.nextIntInRange(range)
+    var tries = 0
+    var running = true
+    while (true){
+        var guess = getUserGuess(maxTries - tries)
+        when {
+            guess == numberToGuess -> return true
+            guess <  numberToGuess -> {
+                println("Your guess is too low")
+            }
+            guess > numberToGuess -> {
+                println("Your guess is too high")
+            }
+        }
+        tries++
+        if (tries >= maxTries) return false
+    }
+}
+
+fun evaluation(b: Boolean){
+    if (b) println("Congratulations, your guess was correct") else println("Sorry, you ran out of tries")
+}
+
+fun getUserGuess(triesLeft: Int) : Int{
+    do{
+        println("Enter your guess between ${range.first} and ${range.last} -- you have $triesLeft tries left")
+        var guess = readLine()!!
+        if (guess.isValidInput()) return guess.toInt() else println("Invalid input")
+    }
+    while (true)
+}
+
+fun String.isValidInput() : Boolean = this.toIntOrNull() != null && this.toInt() in range
+
+fun Random.nextIntInRange(range : IntRange): Int = this.nextInt(range.last - range.first + 1) + range.first
